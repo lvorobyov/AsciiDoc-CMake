f9ef327823b98a6561843eac04ef643c5e4e9708
lvorobjov <lev.vorobjov@rambler.ru>
2020-06-08 21:50:49 +0300

    bib

--- CsoiDoc.cmake
+++ CsoiDoc.cmake
@@ -4,13 +4,8 @@ function(add_doc _source)
     set(_main "")
     if (${ARGC} GREATER 1 AND (${ARGV1} OR ${ARGV1} MATCHES "\\.bib$"))
         # Check is bibtex file presented
-        set(_bib "")
-        if (${ARGV1} MATCHES "\\.bib$")
-            get_filename_component(BIBNAME ${ARGV1} NAME_WE)
-            set(_bib "print qq(\\n\\\\bibliographystyle{ugost2003}\\n\\\\bibliography{${BIBNAME}});")
-        endif ()
         set(_main "BEGIN{print qq(\\\\begin{document}\\n);} \
-                   END{${_bib} print qq(\\n\\\\end{document}\\n);}")
+                   END{print qq(\\n\\\\end{document}\\n);}")
     endif()
     add_custom_command(OUTPUT ${TEX_FILE}
             COMMAND perl -MAsciiDoc -C -lpe "${_main} chomp; to_latex($_, qq(\\n));" < ${_source} > ${TEX_FILE}
